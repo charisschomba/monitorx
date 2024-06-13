f68d971e324f9a8afd420a2fbc2e8a01497c9195
charisschomba <chombachariss@gmail.com>
2024-06-13 12:41:38 +0300

    initial commit

---
+++ src/main/java/com/finserve/africa/monitorx/MonitorXApplication.java
@@ -0,0 +1,13 @@
+package com.finserve.africa.monitorx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finserve/africa/monitorx/MonitorXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finserve.africa.monitorx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
